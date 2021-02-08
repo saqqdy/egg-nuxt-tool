6c105d75275806da538dbaddb4315cd2e3f5caae
saqqdy <saqqdy@qq.com>
2021-02-08 18:49:44 +0800

    update package and add whitePath

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
 # egg-nuxt-tool
 
+[![NPM version][npm-image]][npm-url]
+[![build status][travis-image]][travis-url]
+[![Test coverage][codecov-image]][codecov-url]
+[![David deps][david-image]][david-url]
+[![Known Vulnerabilities][snyk-image]][snyk-url]
+[![npm download][download-image]][download-url]
+
+[npm-image]: https://img.shields.io/npm/v/egg-nuxt-tool.svg?style=flat-square
+[npm-url]: https://npmjs.org/package/egg-nuxt-tool
+[travis-image]: https://travis-ci.org/saqqdy/egg-nuxt-tool.svg?branch=master
+[travis-url]: https://travis-ci.org/saqqdy/egg-nuxt-tool
+[codecov-image]: https://img.shields.io/codecov/c/github/saqqdy/egg-nuxt-tool.svg?style=flat-square
+[codecov-url]: https://codecov.io/github/saqqdy/egg-nuxt-tool?branch=master
+[david-image]: https://img.shields.io/david/saqqdy/egg-nuxt-tool.svg?style=flat-square
+[david-url]: https://david-dm.org/saqqdy/egg-nuxt-tool
+[snyk-image]: https://snyk.io/test/npm/egg-nuxt-tool/badge.svg?style=flat-square
+[snyk-url]: https://snyk.io/test/npm/egg-nuxt-tool
+[download-image]: https://img.shields.io/npm/dm/egg-nuxt-tool.svg?style=flat-square
 [download-url]: https://npmjs.org/package/egg-nuxt-tool
 
 <!--
@@ -10,6 +28,8 @@ Description here.
 
 ```bash
 $ npm i egg-nuxt-tool --save
+// or
+$ yarn add egg-nuxt-tool
 ```
 
 ## Usage
@@ -27,6 +47,11 @@ exports.nuxtTool = {
 ```js
 // {app_root}/config/config.default.js
 exports.nuxtTool = {
+  whitePath: [/^\/java\//, '/public', '/graphql'], // whiteList
+  ...nuxtConfig, // nuxtConfig
+  rootDir: path.join(appInfo.baseDir, 'path/of/nuxtsite/root'),
+  srcDir: path.resolve(appInfo.baseDir, 'path/of/nuxtsite/root'),
+  modulesDir: path.resolve(appInfo.baseDir, 'path/of/nuxtsite/root/node_modules')
 };
 ```
 
--- app/middleware/nuxt-tool.js
+++ app/middleware/nuxt-tool.js
@@ -1,32 +1,39 @@
 const { Nuxt, Builder } = require('nuxt')
+const getType = require('js-cool/lib/getType')
 
 module.exports = (options, app) => {
-	// console.log('egg-nuxt-tool', options, app)
-	const { httpProxy = {}, httpProxyPlus = {}, static = {} } = app.config
+	const { static: staticSrc = {} } = app.config
+	let { whitePath = [] } = options
 	const nuxtRender = new Nuxt(options)
-	const isDev = process.env.NODE_ENV !== 'production'
+	// const isDev = process.env.NODE_ENV !== 'production'
 	// if (isDev) {
 	// 	new Builder(nuxtRender).build()
 	// }
 	return async function (ctx, next) {
 		let flag = false,
+			whitePathType = getType(whitePath),
 			routerArr = []
+		if (whitePathType === 'string') whitePath = [whitePath]
+		// whiteList go next
+		for (let src of whitePath) {
+			const srcType = getType(src)
+			if (srcType === 'string' && ctx.originalUrl.indexOf(src) === 0) return await next()
+			else if (srcType === 'regexp' && src.test(ctx.originalUrl)) return await next()
+		}
+		// router path
 		if (!flag) {
 			routerArr = app.router.stack.map(el => el.path)
 			flag = true
 		}
-		for (const key in httpProxy) {
-			if (ctx.originalUrl.indexOf(key) === 0) return await next()
-		}
-		for (const key in httpProxyPlus) {
-			if (ctx.originalUrl.indexOf(key) === 0) return await next()
-		}
-		if (static.prefix && ctx.path.indexOf('/public') === 0) return await next()
-		if (static.dir) {
-			for (let dir of static.dir) {
+		// single static path
+		if (staticSrc.prefix && ctx.path.indexOf(staticSrc.prefix) === 0) return await next()
+		// muti static path
+		if (staticSrc.dir) {
+			for (let dir of staticSrc.dir) {
 				if (dir.prefix && dir.prefix !== '/' && ctx.path.indexOf(dir.prefix) === 0) return await next()
 			}
 		}
+		// router path
 		for (const route of routerArr) {
 			if (typeof route === 'string' && route === ctx.path) return await next()
 			try {
@@ -36,8 +43,6 @@ module.exports = (options, app) => {
 				throw e
 			}
 		}
-		if (ctx.path === '/graphql') return await next()
-		if (ctx.path.indexOf('/public') === 0) return await next()
 		ctx.status = 200
 		ctx.req.session = ctx.session
 		const { res, req } = ctx
